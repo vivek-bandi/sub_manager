42fa676a5496087b6d15a40f2df78317cbf5923f
vivek-bandi <bandivivek50@gmail.com>
2026-06-05 11:44:37 +0530

    frontend and auth route update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/signup", register);
 router.post("/login", login);
 
 export default router;
